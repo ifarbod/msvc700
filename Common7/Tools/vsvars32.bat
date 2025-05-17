7e4869520824721e5cf538f80d2da9cac3fb4023
iFarbod <ifarbod@outlook.com>
2025-05-17 04:50:13 +0330

    Add msvc700

---
+++ Common7/Tools/vsvars32.bat
@@ -0,0 +1,46 @@
+@rem Set the root of the MSVC installation based on the script's location (https://github.com/itsmattkc/MSVC420/blob/master/bin/VCVARS32.BAT)
+@SET MSDevDir=%~dp0\..\..
+
+@SET VSINSTALLDIR=%MSDevDir%\Common7\IDE
+@SET VCINSTALLDIR=%MSDevDir%
+@SET FrameworkDir=c:\Windows\Microsoft.NET\Framework
+@SET FrameworkVersion=v1.0.3705
+@SET FrameworkSDKDir=%MSDevDir%\FrameworkSDK
+@rem Root of Visual Studio common files.
+
+@if "%VSINSTALLDIR%"=="" goto Usage
+@if "%VCINSTALLDIR%"=="" set VCINSTALLDIR=%VSINSTALLDIR%
+
+@rem
+@rem Root of Visual Studio ide installed files.
+@rem
+@set DevEnvDir=%VSINSTALLDIR%
+
+@rem
+@rem Root of Visual C++ installed files.
+@rem
+@set MSVCDir=%VCINSTALLDIR%\VC7
+
+@rem
+@echo Setting environment for using Microsoft Visual Studio .NET tools.
+@echo (If you also have Visual C++ 6.0 installed and wish to use its tools
+@echo from the command line, run vcvars32.bat for Visual C++ 6.0.)
+@rem
+
+@REM %VCINSTALLDIR%\Common7\Tools dir is added only for real setup.
+
+@set PATH=%DevEnvDir%;%MSVCDir%\BIN;%VCINSTALLDIR%\Common7\Tools;%VCINSTALLDIR%\Common7\Tools\bin\prerelease;%VCINSTALLDIR%\Common7\Tools\bin;%FrameworkSDKDir%\bin;%FrameworkDir%\%FrameworkVersion%;%PATH%;
+@set INCLUDE=%MSVCDir%\ATLMFC\INCLUDE;%MSVCDir%\INCLUDE;%MSVCDir%\PlatformSDK\include\prerelease;%MSVCDir%\PlatformSDK\include;%FrameworkSDKDir%\include;%INCLUDE%
+@set LIB=%MSVCDir%\ATLMFC\LIB;%MSVCDir%\LIB;%MSVCDir%\PlatformSDK\lib\prerelease;%MSVCDir%\PlatformSDK\lib;%FrameworkSDKDir%\lib;%LIB%
+
+@goto end
+
+:Usage
+
+@echo. VSINSTALLDIR variable is not set. 
+@echo.
+@echo SYNTAX: %0
+
+@goto end
+
+:end
---
+++ README.md
@@ -0,0 +1,13 @@
+# Microsoft Visual Studio .NET 2002 (MSVC v7.00 / 13.00)
+
+This is a stripped version that can reliably compile C++. .NET 1.0 is NOT needed.
+
+- Product version: `7.00.9466`
+- Compiler (c1xx.dll) version: `13.00.9466`
+
+```bat
+git clone https://github.com/ifarbod/msvc700.git
+call msvc700\Common7\Tools\vsvars32.bat
+
+:: you can use cl, link, ...
+```
